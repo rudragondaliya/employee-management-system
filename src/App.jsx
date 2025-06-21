bf3a3ec15e0a9850d3f151a6d041b3c2b1255258
rudragondaliya <gondaliyarudra61@gmail.com>
2025-06-21 15:19:42 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,3 @@
-// import React from 'react';
-// import Login from './pages/Login';
-// import { Route, Routes } from 'react-router-dom';
-// import PrivateRoute from './components/Auth/PrivateRoute';
-// import Dashboard from './pages/Dashboard';
-// import EmployeePage from './pages/EmployeePage';
-// import AddEmployee from './pages/AddEmployee';
-// import EmployeeForm from './components/Manager/EmployeeForm';
-// import ManagerToolsPage from './pages/ManagerTool';
-// import SalarySlipPage from './pages/SalarySlipPage';
-// import ChatPage from './pages/Chatpage';
-// import Logout from './pages/Logout';
-// // import AttendanceOverview from './components/Manager/AttendenceOverview';
-
-// const App = () => {
-//   return (
-//     <>
-//     <Routes>
-//       <Route path='/' element={<Login/>}></Route>
-//       <Route path='/dashboard' element={
-//         <PrivateRoute>
-//         <Dashboard/>
-//       </PrivateRoute>}>
-//       </Route>
-//       <Route path='/employee' element={
-//         <PrivateRoute>
-//           <EmployeePage/>
-//         </PrivateRoute>
-//       }></Route>
-//       <Route path='/employee/add' element={<PrivateRoute>
-//         <AddEmployee/>
-//       </PrivateRoute>}></Route>
-//       <Route path="/employee/edit/:id" element={<EmployeeForm />} />
-//       <Route path="/tools" element={<PrivateRoute><ManagerToolsPage /></PrivateRoute>} />
-//       <Route path="/salary-slip" element={<PrivateRoute><SalarySlipPage /></PrivateRoute>} />
-//           <Route path="/chat" element={<ChatPage />} />
-//           <Route path="/logout" element={<Logout
-//            />} />
-//     </Routes>
-      
-//     </>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
@@ -56,7 +9,7 @@ import ManagerToolsPage from './pages/ManagerTool';
 import SalarySlipPage from './pages/SalarySlipPage';
 import ChatPage from './pages/Chatpage';
 import Logout from './pages/Logout';
-import NotFound from './pages/NotFound';
+import NotFound from './pages/Notfound';
 import PrivateRoute from '../src/components/Auth/PrivateRoute'
 
 const App = () => {
